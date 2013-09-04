3d4ccfe06c52ca168b4477c637f0877d2c48c9f4
Wolf Goedel <wolf.goedel@me.com>
2013-09-05 01:02:38 +0200

    fixed browsersupport with new jQuery

--- src/lib/aloha/core.js
+++ src/lib/aloha/core.js
@@ -44,8 +44,10 @@ define([
 		var browser = $.browser;
 		var version = browser.version;
 		return !(
-			// Chrome/Safari 4
-			(browser.webkit && parseFloat(version) < 532.5) ||
+      // Chrome ???
+      (browser.chrome && parseFloat(version) < 10) ||
+      // Safari 4
+      (browser.webkit && !browser.chrome && parseFloat(version) < 532.5) ||
 			// FF 3.5
 			(browser.mozilla && parseFloat(version) < 1.9) ||
 			// IE 7
